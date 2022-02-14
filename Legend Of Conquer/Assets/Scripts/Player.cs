585ba63add9c770ca853e0f0611cf42a9579316c
flaxyars <74737840+emreunal2@users.noreply.github.com>
2022-02-15 01:28:47 +0300

    Player Movement and Idle

--- Legend Of Conquer/Assets/Scripts/Player.cs
+++ Legend Of Conquer/Assets/Scripts/Player.cs
@@ -16,12 +16,27 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveSpeed = 10;
+        
+        float moveSpeed = 5;
+        // Getting inputs
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
         float verticalMovement = Input.GetAxisRaw("Vertical");
+        
+        // Remove dioganal move
+        if (horizontalMovement != 0) verticalMovement = 0;
+
         playerRigidbody.velocity = new Vector2(horizontalMovement*moveSpeed, verticalMovement*moveSpeed);
 
+        // Send input infos to animation
         playerAnimator.SetFloat("MovementX", playerRigidbody.velocity.x);
         playerAnimator.SetFloat("MovementY", playerRigidbody.velocity.y);
+
+        // Setting walking animation 
+        if (horizontalMovement == 1 || horizontalMovement == -1 || verticalMovement == 1 || verticalMovement == -1)
+        {
+            playerAnimator.SetFloat("lastX", horizontalMovement);
+            playerAnimator.SetFloat("lastY", verticalMovement);
+        }
+
     }
 }
